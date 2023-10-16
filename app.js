629788265b46c67570406b3359a9314c98743605
Roberto Alvira <alvira5@hotmail.com>
2023-10-16 00:38:17 -0400

    javascript-segunda interactive

---
+++ app.js
@@ -0,0 +1,10 @@
+var x = document.getElementById("sub-btn");
+var y = document.getElementBy("activate-state");
+var z = document.getElementBy("int-container");
+
+
+function cambio() {
+    div.innerHTML = 
+    
+    
+}
